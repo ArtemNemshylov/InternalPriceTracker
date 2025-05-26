d1edb4a073eb8eea0610258d54f49206c0fa6b77
Nemsh <Artnemshilov@gmail.com>
2025-05-26 15:07:33 +0200

    christinacosmetics

--- src/parsers/registry.py
+++ src/parsers/registry.py
@@ -3,11 +3,13 @@ from src.parsers.lascos import parser as lascos
 from src.parsers.pielcosmetics import parser as pielcosmetics
 from src.parsers.sesderma import parser as sesderma
 from src.parsers.totispharma import parser as totispharma
+from src.parsers.christinacosmetics import parser as christinacosmetics
 
 PARSER_REGISTRY = {
     "decaar": decaar.main,
     "lascos": lascos.main,
     "pielcosmetics": pielcosmetics.main,
     "sesderma": sesderma.main,
-    "totispharma": totispharma.main
+    "totispharma": totispharma.main,
+    "christinacosmetics": christinacosmetics.main
 }
